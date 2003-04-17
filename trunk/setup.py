8e03e84820b372ec6eb360b2aa4a9c386f2b8f9a
Fernando Pérez <fperez.net@gmail.com>
2003-04-17 19:00:46 +0000

    Split windows-specific stuff over to a separate file
    git-svn-id: http://ipython.scipy.org/svn/ipython@47

--- trunk/setup.py
+++ trunk/setup.py
@@ -7,7 +7,7 @@ because options like sdist require other utilities not available under
 Windows."""
 
 #*****************************************************************************
-#       Copyright (C) 2001 Fernando Pérez. <fperez@pizero.colorado.edu>
+#       Copyright (C) 2001 Fernando Pérez. <fperez@colorado.edu>
 #
 #  Distributed under the terms of the GNU Lesser General Public License (LGPL)
 #
@@ -115,123 +115,6 @@ setup(name             = name,
 
 # For Unix users, things end here.
 # Under Windows, do some extra stuff.
-if os_name == 'windows':
-    try:
-        import shutil,pythoncom
-        from win32com.shell import shell
-        import _winreg as wreg
-    except ImportError:
-        print """
-You seem to be missing the PythonWin extensions necessary for automatic
-installation.  You can get them (free) from
-http://starship.python.net/crew/mhammond/
-
-Please see the manual for details if you want to finish the installation by
-hand, or get PythonWin and repeat the procedure.
-
-Press <Enter> to exit this installer."""
-        raw_input()
-        sys.exit()
-
-    #--------------------------------------------------------------------------
-    def make_shortcut(fname,target,args='',start_in='',comment='',icon=None):
-        """Make a Windows shortcut (.lnk) file.
-
-        make_shortcut(fname,target,args='',start_in='',comment='',icon=None)
-
-        Arguments:
-            fname - name of the final shortcut file (include the .lnk)
-            target - what the shortcut will point to
-            args - additional arguments to pass to the target program
-            start_in - directory where the target command will be called
-            comment - for the popup tooltips
-            icon - optional icon file. This must be a tuple of the type 
-            (icon_file,index), where index is the index of the icon you want
-            in the file. For single .ico files, index=0, but for icon libraries
-            contained in a single file it can be >0.
-        """
-
-        shortcut = pythoncom.CoCreateInstance(
-            shell.CLSID_ShellLink, None,
-            pythoncom.CLSCTX_INPROC_SERVER, shell.IID_IShellLink
-        )
-        shortcut.SetPath(target)
-        shortcut.SetArguments(args)
-        shortcut.SetWorkingDirectory(start_in)
-        shortcut.SetDescription(comment)
-        if icon:
-            shortcut.SetIconLocation(*icon)
-        shortcut.QueryInterface(pythoncom.IID_IPersistFile).Save(fname,0)
-
-
-    #--------------------------------------------------------------------------
-    # 'Main' code
-
-    # Find where the Start Menu and My Documents are on the filesystem
-    key = wreg.OpenKey(wreg.HKEY_CURRENT_USER,
-                       r'Software\Microsoft\Windows\CurrentVersion'
-                       r'\Explorer\Shell Folders')
-
-    start_menu_dir = wreg.QueryValueEx(key,'Programs')[0]
-    my_documents_dir = wreg.QueryValueEx(key,'Personal')[0]
-    key.Close()
-
-    # Find where the 'program files' directory is
-    key = wreg.OpenKey(wreg.HKEY_LOCAL_MACHINE,
-                       r'SOFTWARE\Microsoft\Windows\CurrentVersion')
-
-    program_files_dir = wreg.QueryValueEx(key,'ProgramFilesDir')[0]
-    key.Close()
-
-    # File and directory names
-    ip_start_menu_dir = start_menu_dir + '\\IPython'
-    ip_install_dir = program_files_dir + '\\IPython'  # Actual files
-    doc_dir = ip_install_dir+'\\doc'
-    ip_filename = ip_install_dir+'\\IPython_shell.py'
-    pycon_icon = doc_dir+'\\pycon.ico'
-
-    if not os.path.isdir(ip_install_dir):
-        os.mkdir(ip_install_dir)
-
-    # Copy startup script and documentation
-    shutil.copy(sys.prefix+'\\Scripts\\ipython',ip_filename)
-    if os.path.isdir(doc_dir):
-        shutil.rmtree(doc_dir)
-    shutil.copytree('doc',doc_dir)
-
-    # make shortcuts for IPython, html and pdf docs.
-    print '\n\n\nMaking entries for IPython in Start Menu...',
-
-    # Create shortcuts in Programs\IPython:
-    if not os.path.isdir(ip_start_menu_dir):
-        os.mkdir(ip_start_menu_dir)
-    os.chdir(ip_start_menu_dir)
-
-    man_pdf = doc_dir + '\\manual.pdf'
-    man_htm = doc_dir + '\\manual\\manual.html'
-
-    make_shortcut('IPython.lnk',sys.executable, '"%s"' % ip_filename,
-                  my_documents_dir,
-                  'IPython - Enhanced python command line interpreter',
-                  (pycon_icon,0))
-    make_shortcut('Manual in HTML format.lnk',man_htm,'','',
-                  'IPython Manual - HTML format')
-    make_shortcut('Manual in PDF format.lnk',man_pdf,'','',
-                  'IPython Manual - PDF format')
-
-    print """Done.
-
-I created the directory %(ip_install_dir)s. There you will find the
-IPython startup script and manuals.
-
-An IPython menu was also created in your Start Menu, with entries for
-IPython itself and the manual in HTML and PDF formats.
-
-For reading PDF documents you need the freely available Adobe Acrobat
-Reader. If you don't have it, you can download it from:
-http://www.adobe.com/products/acrobat/readstep2.html
-
-Finished with IPython installation. Press Enter to exit this installer.""" % locals()
-    raw_input()
-
-def dummy(): pass  # Stupid fix for emacs' python mode. Don't remove.
+if os_name=='windows':
+    import ipython_win_post_install
+    ipython_win_post_install.run(wait=1)
