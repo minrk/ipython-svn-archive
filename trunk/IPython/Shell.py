e900a5dd92753172c5f19461be33ffd23b998e78
Fernando Pérez <fperez.net@gmail.com>
2004-08-27 22:43:33 +0000

    *** empty log message ***
    git-svn-id: http://ipython.scipy.org/svn/ipython@355

--- trunk/IPython/Shell.py
+++ trunk/IPython/Shell.py
@@ -377,9 +377,9 @@ class MatplotlibShellBase:
 
         # Build matplotlib info banner
         b=('\nWelcome to pylab, a matplotlib-based Python environment.\n'
-           'help(matplotlib) -> generic matplotlib information.\n'
-           'help(matlab)     -> matlab-compatible commands from matplotlib.\n'
-           'help(plotting)   -> plotting commands.\n')
+           '  help(matplotlib) -> generic matplotlib information.\n'
+           '  help(matlab)     -> matlab-compatible commands from matplotlib.\n'
+           '  help(plotting)   -> plotting commands.\n')
 
         return user_ns,b
 
