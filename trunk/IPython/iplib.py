7cdab158642f2a6d6fa19086bd5f0030259415dc
Fernando Pérez <fperez.net@gmail.com>
2005-03-20 00:56:04 +0000

    Back off auto-capture for shell commands, it doesn't work.
    git-svn-id: http://ipython.scipy.org/svn/ipython@547

--- trunk/IPython/iplib.py
+++ trunk/IPython/iplib.py
@@ -576,6 +576,20 @@ class InteractiveShell(code.InteractiveConsole, Logger, Magic):
 
         self.dir_stack = [os.getcwd().replace(self.home_dir,'~')]
 
+        # Function to call the underlying shell.  Similar to os.system, but it
+        # doesn't return a value, and it allows interpolation of variables in
+        # the user's namespace.
+        self.system = lambda cmd: shell(str(ItplNS(cmd.replace('#','\#'),
+                                                   self.user_ns)),
+                                        header='IPython system call: ',
+                                        verbose=self.rc.system_verbose)
+        # Similar one for getoutputerror.
+        self.getoutputerror = lambda cmd: \
+                              getoutputerror(str(ItplNS(cmd.replace('#','\#'),
+                                                        self.user_ns)),
+                                             header='IPython system call: ',
+                                             verbose=self.rc.system_verbose)
+ 
         # RegExp for splitting line contents into pre-char//first
         # word-method//rest.  For clarity, each group in on one line.
 
@@ -978,36 +992,6 @@ want to merge them back into the new files.""" % locals()
 
         # Configure auto-indent for all platforms
         self.set_autoindent(self.rc.autoindent)
-
-    def system(self,cmd):
-        """Function to call the underlying shell.
-
-        Similar to os.system, but it doesn't return a value, and it allows
-        interpolation of variables in the user's namespace.
-
-        It also sets the _o, _e variables in the user's namespace.  These
-        always contain the last values for standard output and error, as
-        strings.  Furthermore, these special variables hold a .l attribute
-        which automatically returns them as a list, allowing easy looping over
-        their contents."""
-        
-        out,err = self.getoutputerror(cmd)
-        if out:
-            print >> Term.cout, out
-        if err:
-            print >> Term.cerr, err
-        # Now, set _o and _e.
-        self.user_ns['_o'] = LSString(out)
-        self.user_ns['_e'] = LSString(err)
-        
-    def getoutputerror(self,cmd):
-        """Call a command and return stdout/err, allowing variable
-        interpolation in the user's namespace."""
-        
-        return getoutputerror(str(ItplNS(cmd.replace('#','\#'),
-                                         self.user_ns)),
-                              header='IPython system call: ',
-                              verbose=self.rc.system_verbose)
 
     def showsyntaxerror(self, filename=None):
         """Display the syntax error that just occurred.
