213dab6b930915da116790a391adadf80694ed35
Robert Kern <robert.kern@gmail.com>
2005-07-18 16:19:19 +0000

    Cell branch
    git-svn-id: http://ipython.scipy.org/svn/ipython@639

---
+++ google-rkern/branches/cell/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name = "notabene",
+      version = "0.0",
+      packages = find_packages(),
+      install_requires = ['lxml>=0.7',
+                          'elementtree',
+                         ],
+      extras_require = {'reST': ['docutils'],
+                       },
+      author = "Robert Kern",
+      author_email = "rkern@ucsd.edu",
+      license = "BSD",
+)
