64cf7bf9dd7e5c4aad1be115a50840fa2fe22a51
tzanko <tzanko@5d925edf-2efb-0310-891d-d227fdbe9301>
2005-07-31 10:20:35 +0000

    nbshell:
    The previous revision was broken. 
    
    git-svn-id: http://ipython.scipy.org/svn/ipython@663

--- nbshell/trunk/nbshell/utils.py
+++ nbshell/trunk/nbshell/utils.py
@@ -1,35 +1,21 @@
 """ Utility functions and classes """
 
 #This method is temporary until notebook.py is fixed
+
 from notabene import notebook
 from notabene.notebook import *
-def default_sheet(self, specials=True, figures=True,
-                  logid='default-log'):
-    """Generate a default sheet that has all inputs and outputs.
 
-    If specials is True, replace inputs that are ipython specials with their
-        ipython form.
-    If figures is True, include figures.
-    """
-    log = self.get_log(logid)
-    cells = sorted((Cell(x) for x in log), lambda x: x.number)
-    figured = dict((int(x.get('number')), x) for x in log.xpath('./figure'))
-    
-    sheet = ET.Element('sheet')
-    block = ET.SubElement(sheet, 'ipython-block', logid=logid)
-    for cell in cells:
-        for subcell in cell.get_sheet_tags(specials):
-            block.append(subcell)
-
-        if figures and cell.number in figured:
-            # add figures to the sheet, not the block
-            ET.SubElement(sheet, 'ipython-figure',
-                number=str(cell.number), logid=logid)
-            # start a new block
-            block = ET.SubElement(sheet, 'ipython-block', logid=logid)
-
-    return sheet
-
-notebook.Notebook.default_sheet = default_sheet
+def get_sheet_tags(self, do_specials=False):
+    if do_specials and hasattr(self, 'special'):
+        yield ET.Element('ipython-special',
+            number=str(self.number))
+    else:
+        yield ET.Element('ipython-input',
+            number=str(self.number))
+    for tag in ('traceback', 'stdout', 'stderr', 'output'):
+        if hasattr(self, tag):
+            yield ET.Element('ipython-%s'%tag,
+                number=str(self.number))
+notebook.Cell.get_sheet_tags = get_sheet_tags
 del notebook
 
