32e586f150cdb9466d66b57a6e2e572668182322
Robert Kern <robert.kern@gmail.com>
2005-09-07 23:02:06 +0000

    New distutils-only setup.py
    git-svn-id: http://ipython.scipy.org/svn/ipython@809

--- nbdoc/trunk/setup.py
+++ nbdoc/trunk/setup.py
@@ -1,14 +1,38 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
-setup(name = "notabene",
-      version = "0.0",
-      packages = find_packages(),
-      install_requires = [#'lxml>=0.7',
-                         # 'elementtree',
-                         ],
-      extras_require = {'reST': ['docutils'],
-                       },
+setup(name = "nbdoc",
+      version = "0.1",
+      packages = ['notabene', 'notabene.testing', 'notabene.normalization'],
+      package_data = {'notabene': ['xsl/VERSION',
+                                   'xsl/README*',
+                                   'xsl/*/*.*',
+                                   'xsl/*/*/*.*',
+                                   'xsl/*/*/*/*.*',
+                                   ]},
+      description = "ipython notebook document generation",
+      long_description = 
+"""notabene is the document generation component for the ipython notebook
+project.
+""",
       author = "Robert Kern",
-      author_email = "rkern@ucsd.edu",
-      license = "BSD",
+      author_email = "rkern@enthought.com",
+      maintainer = "Toni Alatalo",
+      maintainer_email = "antont@an.org",
+      classifiers = [f.strip() for f in """
+        Development Status :: 3 - Alpha
+        Intended Audience :: Developers
+        Intended Audience :: Science/Research
+        License :: OSI Approved :: BSD License
+        Operating System :: MacOS :: MacOS X
+        Operating System :: Microsoft :: Windows
+        Operating System :: POSIX
+        Programming Language :: Python
+        Topic :: Scientific/Engineering
+        Topic :: Software Development :: Documentation
+        Topic :: Software Development :: Libraries :: Python Modules
+        Topic :: Text Processing :: Markup :: XML
+        Topic :: Text Processing :: Markup :: HTML
+        Topic :: Text Processing :: Markup :: LaTeX
+        """.splitlines() if f.strip()],
 )
+
