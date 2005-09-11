2faa2b67900daf6c505e24c0765e35f923c88fdd
Robert Kern <robert.kern@gmail.com>
2005-09-11 01:05:00 +0000

    Removed setuptools stuff.
    git-svn-id: http://ipython.scipy.org/svn/ipython@826

--- nbshell/trunk/setup.py
+++ nbshell/trunk/setup.py
@@ -1,11 +1,25 @@
-import ez_setup
-ez_setup.use_setuptools()
-
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
     name = "nbshell",
+    author = "Tzanko Matev",
+    author_email = "tsanko@gmail.com",
     version = "0.1",
-    #packages = ['nbshell']
-    packages = find_packages()
+    packages = ['nbshell', 'nbshell.plotting_backends'],
+    classifiers = [f.strip() for f in """
+        Development Status :: 3 - Alpha
+        Intended Audience :: Developers
+        Intended Audience :: Science/Research
+        License :: OSI Approved :: BSD License
+        Operating System :: MacOS :: MacOS X
+        Operating System :: Microsoft :: Windows
+        Operating System :: POSIX
+        Programming Language :: Python
+        Topic :: Scientific/Engineering
+        Topic :: Software Development :: Documentation
+        Topic :: Software Development :: Libraries :: Python Modules
+        Topic :: Text Processing :: Markup :: XML
+        Topic :: Text Processing :: Markup :: HTML
+        Topic :: Text Processing :: Markup :: LaTeX
+        """.splitlines() if f.strip()],
 )
