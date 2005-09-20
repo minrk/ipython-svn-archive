a1cbb363b75f05070ce1b0a4dd310cbb50335343
Fernando Pérez <fperez.net@gmail.com>
2005-09-20 23:51:40 +0000

    Temporary setup file for kernel as a package
    git-svn-id: http://ipython.scipy.org/svn/ipython@884

---
+++ ipython/branches/chainsaw/ipython/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Setup script for ipython's kernel package.
+"""
+
+#*****************************************************************************
+#       Copyright (C) 2001-2005 Fernando Perez <fperez@colorado.edu>
+#
+#  Distributed under the terms of the BSD License.  The full license is in
+#  the file COPYING, distributed as part of this software.
+#*****************************************************************************
+
+from distutils.core import setup
+
+# Call the setup() routine which does most of the work
+setup(name             = 'ipkernel',
+      version          = '0.1',
+      description      = 'ipython kernel - temporary package',
+      long_description = 'ipython kernel - temporary package',
+      author           = 'Brian / Fernando',
+      author_email     = 'Brian / Fernando',
+      url              = 'http://ipython.scipy.org',
+      license          = 'BSD',
+      packages         = ['kernel'],
+      )
