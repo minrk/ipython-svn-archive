ed4f12506497830eda00f38cb7eac2141e120b8d
Fernando Pérez <fperez.net@gmail.com>
2006-01-30 06:47:47 +0000

    fix typo in release file
    git-svn-id: http://ipython.scipy.org/svn/ipython@1105

--- ipython/branches/0.7.1/IPython/Release.py
+++ ipython/branches/0.7.1/IPython/Release.py
@@ -22,7 +22,7 @@ name = 'ipython'
 # because bdist_rpm does not accept dashes (an RPM) convention, and
 # bdist_deb does not accept underscores (a Debian convention).
 
-version = '0.7.1,fix1'
+version = '0.7.1.fix1'
 
 revision = '$Revision$'
 
