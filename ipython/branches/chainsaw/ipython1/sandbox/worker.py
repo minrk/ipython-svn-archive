aec9f0d96dce333704a69ad47ae6e7767ee58e3c
Brian Granger <ellisonbg@gmail.com>
2006-02-06 07:11:49 +0000

    Added a basic Worker in sandbox/worker.py that provide a BackgroundJob() like interface to a RemoteKernel.  Most importantly, it is non-blocking and you can set timeouts on it.  Much more needs to be done, but this is a start.
    git-svn-id: http://ipython.scipy.org/svn/ipython@1129

---
+++ ipython/branches/chainsaw/ipython1/sandbox/worker.py
@@ -0,0 +1,41 @@
+import threading
+import ipython1.kernel1p.kernelclient as kc
+import Queue
+        
+class Worker(threading.Thread):
+    """A Threaded Worker for the IPython Kernel.
+    
+    The important feature is that the result() method takes a timeout
+    or can be set to not block if there is not a result ready.  It takes
+    the same arguments as Queue.get().
+    
+    WARNING:  There is currently no way of killing the thread.
+    
+    >>> w = Worker(kernel_addr)
+    >>> w.start()
+    >>> w.call('f(a,b)',{'a':10,'b':20})   # f must be defined on the kernel
+    >>> w.result()
+    ('f(a,b)', {'a': 10, 'b': 20}, 2353)
+    """
+
+    def __init__(self,addr):
+        self.addr = addr
+        self.kernel = kc.RemoteKernel(addr)
+        self.kernel.connect()
+        self.qin = Queue.Queue()
+        self.qout = Queue.Queue()
+        threading.Thread.__init__(self)
+        
+    def run(self):
+        while True:
+            func_or_exp, glob = self.qin.get()
+            self.kernel.update(glob)
+            self.kernel.execute('result = %s' % func_or_exp)
+            result = self.kernel.pull('result')
+            self.qout.put((func_or_exp,glob,result))
+            
+    def call(self, func_or_exp, glob):
+        self.qin.put((func_or_exp, glob))
+        
+    def result(self,block=True,timeout=None):
+        return self.qout.get(block=block, timeout=timeout)
