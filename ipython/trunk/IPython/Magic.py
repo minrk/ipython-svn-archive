6eb030da6c53dc11048ac148b7c2221336edf33f
Fernando Pérez <fperez.net@gmail.com>
2006-03-12 09:30:31 +0000

    fix macro n1-n2 syntax, which had broken.
    git-svn-id: http://ipython.scipy.org/svn/ipython@1203

--- ipython/trunk/IPython/Magic.py
+++ ipython/trunk/IPython/Magic.py
@@ -1687,7 +1687,7 @@ Currently the magic system has the following functions:\n"""
 
           In [60]: exec In[44:48]+In[49]"""
 
-        opts,args = self.parse_options(parameter_s,'r')
+        opts,args = self.parse_options(parameter_s,'r',mode='list')
         name,ranges = args[0], args[1:]
         #print 'rng',ranges  # dbg
         lines = self.extract_input_slices(ranges,opts.has_key('r'))
@@ -1717,7 +1717,7 @@ Currently the magic system has the following functions:\n"""
         It adds a '.py' extension to the file if you don't do so yourself, and
         it asks for confirmation before overwriting existing files."""
 
-        opts,args = self.parse_options(parameter_s,'r', mode = 'list')
+        opts,args = self.parse_options(parameter_s,'r',mode='list')
         fname,ranges = args[0], args[1:]
         if not fname.endswith('.py'):
             fname += '.py'
