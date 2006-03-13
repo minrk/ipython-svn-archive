6717ae3d90c70fb2909e48e392e526a623d3fb6d
Fernando Pérez <fperez.net@gmail.com>
2006-03-13 01:19:31 +0000

    Update to support SVN Gnuplot.py.
    git-svn-id: http://ipython.scipy.org/svn/ipython@1210

--- ipython/trunk/IPython/Gnuplot2.py
+++ ipython/trunk/IPython/Gnuplot2.py
@@ -99,10 +99,16 @@ if Gnuplot_ori.__version__ <= '1.6':
     # in any version.
     _FileClass = File
 
-else:  # Gnuplot.py version 1.7 and greater
+elif Gnuplot_ori.__version__ =='1.7':
     _FileClass = _BaseFileItem = PlotItems._FileItem
     _BaseTempFileItem = PlotItems._TempFileItem
     File = PlotItems.File
+
+else:  # changes in the newer version (svn as of March'06)
+     _FileClass = _BaseFileItem = PlotItems._FileItem
+     _BaseTempFileItem = PlotItems._NewFileItem
+     File = PlotItems.File
+
 
 # Now, we can add our generic code which is version independent
 
