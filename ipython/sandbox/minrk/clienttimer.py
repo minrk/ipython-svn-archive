292b3fc7a5b27caf4c3ae90e437ff1286bccb461
Min Ragan-Kelley <benjaminrk@gmail.com>
2006-07-28 19:25:04 +0000

    git-svn-id: http://ipython.scipy.org/svn/ipython@1504

--- ipython/sandbox/minrk/clienttimer.py
+++ ipython/sandbox/minrk/clienttimer.py
@@ -5,24 +5,28 @@ import time, sys
 
 from ipython1.kernel.controllerclient import RemoteController
 
-rc = RemoteController(('127.0.0.1', 10105))
+rc = RemoteController(('24.6.186.70', 10105))
 rc.connect()
 nonblock = ''
 block = ''
-
+c = 1
+b = 2
 for i in range(8):
-    time.sleep(1)
-    start  = time.time()
-    rc.execute('c=1', block=True)
-    split1 = time.time()
+#    time.sleep(1)
+#    start  = time.time()
+#    rc.execute('c=%i' %c, block=True)
+#    split1 = time.time()
     time.sleep(1)
     split2 = time.time()
-    rc.execute('b=2')
+    rc.execute('b=%i' %b)
     stop = time.time()
-    block += "%.0f " %(1000*(split1-start))
-    nonblock += "%.0f " %(1000*(stop-split2))
+#    block += "%.1f " %(1000*(split1-start))
+    nonblock += "%.2f " %(1000*(stop-split2))
+#    assert(rc.pull('c', 0) is c)
+#    assert(rc.pull('b', 0) is b)
+#    c+=1
+    b+=2
 
-
-print "block (in ms): ", block
+#print "block (in ms): ", block
 print "nonblock (in ms): ", nonblock
 
