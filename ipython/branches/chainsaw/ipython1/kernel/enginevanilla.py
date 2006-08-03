09b299c42e54311ebff39b9393ceb18844caa0f7
Brian Granger <ellisonbg@gmail.com>
2006-08-03 04:39:01 +0000

    Created docs for the vanilla engine to controller protocol.
    git-svn-id: http://ipython.scipy.org/svn/ipython@1537

--- ipython/branches/chainsaw/ipython1/kernel/enginevanilla.py
+++ ipython/branches/chainsaw/ipython1/kernel/enginevanilla.py
@@ -1,5 +1,8 @@
 from zope.interface import Interface, implements
 from twisted.python import components
+from twisted.internet import protocol, reactor, defer
+from twisted.protocols import basic
+
 
 from ipython1.kernel.engineservice import EngineService, IEngine, Command
 from ipython1.kernel.controllerservice import ControllerService
@@ -7,16 +10,86 @@ from ipython1.kernel.controllerservice import ControllerService
 # Engine side of things
 
 class IVanillaEngineClientProtocol(Interface):
+    pass
     
-class VanillaEngineClientProtocol():
+class VanillaEngineClientProtocol(basic.Int32StringReceiver):
     
     implements(IVanillaEngineClientProtocol)
 
-class IVanillaEngineClientFactory(Interface):
-    """This is what the server factory should look like"""
+    def connectionMade(self):
+        self.transport.setTcpNoDelay(True)
+        desiredID = self.factory.getID()
+        if desiredID is not None:
+            self.sendString("REGISTER %i" % desiredID)
+        else:
+            self.sendString("REGISTER")
+        self.state = 'REGISTERING'
 
-class VanillaEngineClientFactoryFromEngineService():
-    pass
+    def stringReceived(self, msg):
+        splitLine = msg.split(' ', 1)
+        if len(splitLine) == 1:
+            cmd = splitLine[0]
+            args = None
+        else:
+            cmd = splitLine[0]
+            args = splitLine[1:]
+        
+        
+    # Handlers go here
+    
+    
+    
+class IVanillaEngineClientFactory(IEngine):
+    
+    def getID():
+        
+    def setID(id):
+
+class VanillaEngineClientFactoryFromEngineService(protocol.ClientFactory):
+    
+    implements(IVanillaEngineClientFactory)
+    
+    protocol = VanillaEngineClientProtocol
+    
+    def __init__(self, service):
+        self.service = service
+        
+    def getID(self):
+        return self.service.id
+        
+    def setID(self, id):
+        return self.service.id = id
+        
+    def execute(self, lines):
+        return self.service.execute(lines)
+        
+    def push(self, **namespace):
+        return self.service.push(**namespace)
+
+    def pushPickle(pickledNamespace):
+        return self.service.pushPickle(pickledNamespace)
+
+    def pull(*keys):
+        return self.service.pull(*keys)
+
+    def pullPickle(*keys):
+        return self.service.pullPickle(*keys)
+
+    def getCommand(i=None):
+        return self.service.getCommand(i)
+
+    def getLastCommandIndex():
+        return self.service.getLastCommandIndex()
+
+    def reset():
+        return self.service.reset()
+
+    def kill():
+        return self.service.kill()
+
+    def status():
+        return self.service.status()
+        
     
 components.registerAdapter(VanillaEngineClientFactoryFromEngineService,
                            EngineService,
@@ -27,22 +100,27 @@ components.registerAdapter(VanillaEngineClientFactoryFromEngineService,
 
 class IVanillaEngineServerProtocol(Interface):
     
-class VanillaEngineServerProtocol():
+class VanillaEngineServerProtocol(basic.Int32StringReceiver):
     
     implements(IVanillaEngineServerProtocol)
 
 class IVanillaEngineServerFactory(Interface):
     """This is what the client factory should look like"""
 
-class VanillaEngineServerFactoryFromControllerService():
-    pass
+class VanillaEngineServerFactoryFromControllerService(protocol.ServerFactory):
+    
+    implements(IVanillaEngineServerFactory)
+    
+    protocol = VanillaEngineServerProtocol
     
 components.registerAdapter(VanillaEngineServerFactoryFromControllerService,
                            ControllerService,
                            IVanillaEngineServerFactory)
     
 class EngineFromVanillaEngineServerProtocol():
-    pass
+
+    implements(IEngine)
+    
     
 components.registerAdapter(EngineFromVanillaEngineServerProtocol,
                            VanillaEngineServerProtocol,
