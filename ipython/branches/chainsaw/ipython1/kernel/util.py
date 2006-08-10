7449d771b274db43df319d264c14e9930b1ce835
Min Ragan-Kelley <benjaminrk@gmail.com>
2006-08-10 22:26:08 +0000

    import defer
    git-svn-id: http://ipython.scipy.org/svn/ipython@1598

--- ipython/branches/chainsaw/ipython1/kernel/util.py
+++ ipython/branches/chainsaw/ipython1/kernel/util.py
@@ -6,8 +6,9 @@ util.py
 Created by Brian Granger on 2006-08-10.
 Copyright (c) 2006 __MyCompanyName__. All rights reserved.
 """
+from twisted.internet import defer
 
-#from twisted.defer.gatherresults/_parseDlist
+#from twisted.internet.defer.gatherresults/_parseDlist
 def parseResults(results):
     return [x[1] for x in results]
 
