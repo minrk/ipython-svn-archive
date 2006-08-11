9cb41e304fe5be89a600ea560acc7a5c12f82014
Brian Granger <ellisonbg@gmail.com>
2006-08-11 18:01:37 +0000

    Added file for base class tests for IEngineComplete implementers
    git-svn-id: http://ipython.scipy.org/svn/ipython@1609

---
+++ ipython/branches/chainsaw/ipython1/test/completeenginetest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+completeenginetest.py
+
+Created by Brian Granger on 2006-08-11.
+Copyright (c) 2006 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
