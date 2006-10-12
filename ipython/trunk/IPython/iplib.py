4426c79c19ee66d745b18fd792afa579d6402bd6
Ville M. Vainio <vivainio@gmail.com>
2006-10-12 21:38:00 +0000

    prevent cyclic alias definitions
    git-svn-id: http://ipython.scipy.org/svn/ipython@1822

--- ipython/trunk/IPython/iplib.py
+++ ipython/trunk/IPython/iplib.py
@@ -57,7 +57,7 @@ import tempfile
 import traceback
 import types
 import pickleshare
-
+from sets import Set
 from pprint import pprint, pformat
 
 # IPython's own modules
@@ -1598,9 +1598,16 @@ want to merge them back into the new files.""" % locals()
         
         """
         line = fn + " " + rest
+        
+        done = Set()
         while 1:
             pre,fn,rest = self.split_user_input(line)
             if fn in self.alias_table:
+                if fn in done:
+                    warn("Cyclic alias definition, repeated '%s'" % fn)
+                    return ""
+                done.add(fn)
+
                 l2 = self.transform_alias(fn,rest)
                 # dir -> dir 
                 if l2 == line:
