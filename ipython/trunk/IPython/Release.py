a2b99be65e9ea7b0b9040a839c7af8d511158c0c
Ville M. Vainio <vivainio@gmail.com>
2006-11-29 08:01:37 +0000

    inc revnum in Release.py, strip M from revnum
    git-svn-id: http://ipython.scipy.org/svn/ipython@1953

--- ipython/trunk/IPython/Release.py
+++ ipython/trunk/IPython/Release.py
@@ -22,11 +22,11 @@ name = 'ipython'
 # because bdist_rpm does not accept dashes (an RPM) convention, and
 # bdist_deb does not accept underscores (a Debian convention).
 
-revision = '1939'
+revision = '1952M'
 
 #version = '0.7.3.svn' 
 
-version = '0.7.3.svn.r' + revision
+version = '0.7.3.svn.r' + revision.rstrip('M')
 
 
 description = "An enhanced interactive Python shell."
