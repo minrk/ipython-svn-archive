afb2e18c602bdc5d7b9044651775bc4409e29709
Ville M. Vainio <vivainio@gmail.com>
2006-12-07 18:14:03 +0000

    do export in make_tarball
    git-svn-id: http://ipython.scipy.org/svn/ipython@1968

--- ipython/trunk/IPython/Release.py
+++ ipython/trunk/IPython/Release.py
@@ -22,11 +22,11 @@ name = 'ipython'
 # because bdist_rpm does not accept dashes (an RPM) convention, and
 # bdist_deb does not accept underscores (a Debian convention).
 
-revision = '1964M'
+revision = '1967'
 
 #version = '0.7.3.svn' 
 
-version = '0.7.3.svn.r' + revision.rstrip('M')
+version = '0.7.3b2.r' + revision.rstrip('M')
 
 
 description = "An enhanced interactive Python shell."
