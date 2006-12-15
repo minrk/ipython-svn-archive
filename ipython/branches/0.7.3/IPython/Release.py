c9370effe0677e8656726a46096f20d0f5f7df74
Ville M. Vainio <vivainio@gmail.com>
2006-12-15 16:29:05 +0000

    update Release to 0.7.3. This version should become FINAL
    git-svn-id: http://ipython.scipy.org/svn/ipython@2004

--- ipython/branches/0.7.3/IPython/Release.py
+++ ipython/branches/0.7.3/IPython/Release.py
@@ -22,12 +22,11 @@ name = 'ipython'
 # because bdist_rpm does not accept dashes (an RPM) convention, and
 # bdist_deb does not accept underscores (a Debian convention).
 
-revision = '2001M'
+revision = '2004'
 
-#version = '0.7.3.svn' 
+version = '0.7.3'
 
-version = '0.7.3rc2.r' + revision.rstrip('M')
-
+#version = '0.7.3rc2.r' + revision.rstrip('M')
 
 description = "An enhanced interactive Python shell."
 
