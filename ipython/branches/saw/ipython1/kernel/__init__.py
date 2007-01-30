811abe673e295cd57ca38c7dd552b0386de7de3b
Min Ragan-Kelley <benjaminrk@gmail.com>
2007-01-30 17:03:07 +0000

    fixed my name in headers
    git-svn-id: http://ipython.scipy.org/svn/ipython@2057

--- ipython/branches/saw/ipython1/kernel/__init__.py
+++ ipython/branches/saw/ipython1/kernel/__init__.py
@@ -15,7 +15,7 @@ An end user should probably begin by looking at the `kernel.api` module.
 #-------------------------------------------------------------------------------
 #       Copyright (C) 2005  Fernando Perez <fperez@colorado.edu>
 #                           Brian E Granger <ellisonbg@gmail.com>
-#                           Benjamin Ragan-Kelly <<benjaminrk@gmail.com>>
+#                           Benjamin Ragan-Kelley <benjaminrk@gmail.com>
 #
 #  Distributed under the terms of the BSD License.  The full license is in
 #  the file COPYING, distributed as part of this software.
