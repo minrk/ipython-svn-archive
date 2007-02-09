3b0cb7d008e2604ade13ce4996cb2ea82c8bd1a4
Fernando Pérez <fperez.net@gmail.com>
2007-02-09 21:28:58 +0000

    Minor change so that ipython starts correctly even if called via 'python -S', which avoids loading site.py
    git-svn-id: http://ipython.scipy.org/svn/ipython@2093

--- ipython/trunk/IPython/ipmaker.py
+++ ipython/trunk/IPython/ipmaker.py
@@ -20,17 +20,22 @@ __author__  = '%s <%s>' % Release.authors['Fernando']
 __license__ = Release.license
 __version__ = Release.version
 
-credits._Printer__data = """
-Python: %s
+try:
+    credits._Printer__data = """
+    Python: %s
 
-IPython: Fernando Perez, Janko Hauser, Nathan Gray, and many users.
-See http://ipython.scipy.org for more information.""" \
-% credits._Printer__data
+    IPython: Fernando Perez, Janko Hauser, Nathan Gray, and many users.
+    See http://ipython.scipy.org for more information.""" \
+    % credits._Printer__data
 
-copyright._Printer__data += """
+    copyright._Printer__data += """
 
-Copyright (c) 2001-2004 Fernando Perez, Janko Hauser, Nathan Gray.
-All Rights Reserved."""
+    Copyright (c) 2001-2004 Fernando Perez, Janko Hauser, Nathan Gray.
+    All Rights Reserved."""
+except NameError:
+    # Can happen if ipython was started with 'python -S', so that site.py is
+    # not loaded
+    pass
 
 #****************************************************************************
 # Required modules
