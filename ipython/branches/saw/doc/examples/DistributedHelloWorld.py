bfb37b10e0793dff30bab4b110e141e3d2d43101
Min Ragan-Kelley <benjaminrk@gmail.com>
2007-06-30 04:25:52 +0000

    updated HelloWorld to result.ns api
    git-svn-id: http://ipython.scipy.org/svn/ipython@2472

--- ipython/branches/saw/doc/examples/DistributedHelloWorld.py
+++ ipython/branches/saw/doc/examples/DistributedHelloWorld.py
@@ -15,4 +15,4 @@ ipc.execute('all', 'import time')
 helloTaskId = rc.run(kernel.Task('time.sleep(3) ; word = "Hello,"', resultNames=['word']))
 worldTaskId = rc.run(kernel.Task('time.sleep(3) ; word = "World!"', resultNames=['word']))
 
-print rc.getTaskResult(helloTaskId).word, rc.getTaskResult(worldTaskId).word
+print rc.getTaskResult(helloTaskId).ns.word, rc.getTaskResult(worldTaskId).ns.word
