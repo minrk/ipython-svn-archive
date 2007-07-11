5703fdcca0cd54732baf538a065592d99297a09e
Brian Granger <ellisonbg@gmail.com>
2007-07-11 22:26:16 +0000

    Fixing a few things in models.py
    git-svn-id: http://ipython.scipy.org/svn/ipython@2495

--- ipython/branches/saw/ipython1/notebook/models.py
+++ ipython/branches/saw/ipython1/notebook/models.py
@@ -9,54 +9,53 @@ metadata.engine.echo = True
 
 # Tables
 
-nodes_table = Table('nodes', metadata,
-    Column('node_id', Integer, primary_key=True),
-    Column('parent_id', Integer, ForeignKey('nodes.node_id')),
+nodesTable = Table('nodes', metadata,
+    Column('nodeID', Integer, primary_key=True),
+    Column('parentID', Integer, ForeignKey('nodes.nodeID')),
 )
 
-cells_table = Table('cells', metadata,
-    Column('cell_id', Integer, primary_key=True),
-    Column('created', DateTime),
-    Column('modified', DateTime),
-    Column('node_id', Integer, ForeignKey('nodes.node_id')),
-    Column('sequence_number', Integer)    
+cellsTable = Table('cells', metadata,
+    Column('cellID', Integer, primary_key=True),
+    Column('dateCreated', DateTime),
+    Column('dateModified', DateTime),
+    Column('nodeID', Integer, ForeignKey('nodes.nodeID')),
+    Column('sequenceNumber', Integer)    
     )
 
-input_cells_table = Table('input_cells', metadata,
-    Column('cell_id', Integer, ForeignKey('cells.cell_id'), unique=True),
+inputCellsTable = Table('inputCells', metadata,
+    Column('cellID', Integer, ForeignKey('cells.cellID'), primary_key=True),
     Column('input', String()),
     Column('output', String()) 
 )
 
-text_cells_table = Table('text_cells', metadata,
-    Column('cell_id', Integer, ForeignKey('cells.cell_id'), unique=True),
-    Column('text_data', String()),
+textCellsTable = Table('textCells', metadata,
+    Column('cellID', Integer, ForeignKey('cells.cellID'), primary_key=True),
+    Column('textData', String()),
 )
 
-notebooks_table = Table('notebooks', metadata,
-    Column('notebook_id', Integer, primary_key=True),
-    Column('user_id', Integer, ForeignKey('users.user_id')),
-    Column('created', DateTime),
-    Column('modified', DateTime),
-    Column('root_id', Integer, ForeignKey('nodes.node_id')),    
+notebooksTable = Table('notebooks', metadata,
+    Column('notebookID', Integer, primary_key=True),
+    Column('userID', Integer, ForeignKey('users.userID')),
+    Column('dateCreated', DateTime),
+    Column('dateModified', DateTime),
+    Column('rootID', Integer, ForeignKey('nodes.nodeID')),    
 )
     
-users_table = Table('users', metadata,
-    Column('user_id', Integer, primary_key=True),
+usersTable = Table('users', metadata,
+    Column('userID', Integer, primary_key=True),
     Column('username', String(40)),
     Column('email', String(40)),
-    Column('created', DateTime),
-    Column('modified', DateTime)
+    Column('dateCreated', DateTime),
+    Column('dateModified', DateTime)
 )
 
 def createTables():
-    nodes_table.create()
-    cells_table.create()
-    notebooks_table.create()
-    users_table.create()
-    input_cells_table.create()
-    text_cells_table.create()
-    
+    nodesTable.create()
+    cellsTable.create()
+    inputCellsTable.create()
+    textCellsTable.create()
+    notebooksTable.create()
+    usersTable.create()
 
 # Mappers
 
@@ -65,85 +64,58 @@ class Node(object):
 
 class Created(object):
     def __init__(self):
-        self.created = datetime.datetime.now()
+        self.dateCreated = datetime.datetime.now()
 
 class Modified(object):
-    def setModified(self):
-        self.modified = datetime.datetime.now()
+    def touchModified(self):
+        self.dateModified = datetime.datetime.now()
 
 class Timestamper(Created, Modified):
     def __init__(self):
         super(Timestamper, self).__init__()
+        self.touchModified()
 
 class Cell(Timestamper):
-    pass
+    
+    def __init__(self):
+        super(Cell, self).__init__()
 
 class InputCell(Cell):
-    pass
-    # def __init__(self,input='',output=''):
-    #     super(Cell, self).__init__()
-    #     self.input = input
-    #     self.output = output
-
-    # def __repr__(self):
-    #     s = """Cell ID: %d
-    #     Input: %s
-    #     Output: %s
-    #     Created: %s""" 
-    #     return s % (self.cell_id, 
-    #                 self.input, 
-    #                 self.output,
-    #                 self.created.ctime())
+    
+    def __init__(self):
+        super(InputCell, self).__init__()
 
 class TextCell(Cell):
-    pass
+
+    def __init__(self):
+        super(TextCell, self).__init__()
 
 class User(Timestamper):
-    pass
-    # def __init__(self, name='', email=''):
-    #     super(User, self).__init__()
-    #     self.username = name
-    #     self.email = email
-    #     
-    # def __repr__(self):
-    #     s = """User ID: %d
-    #     Username: %s
-    #     Email: %s
-    #     Created: %s"""
-    #     return s % (self.user_id,
-    #                 self.username, 
-    #                 self.email,
-    #                 self.created.ctime())
+    
+    def __init__(self):
+        super(User, self).__init__()
 
 class Notebook(Timestamper):
-    pass
-    # def __repr__(self):
-    #     super(Notebook, self).__init__()
-    #     s = """Notebook ID: %d
-    #     User ID: %d
-    #     Created: %s""" 
-    #     return s % (self.notebook_id,
-    #                 self.user_id,
-    #                 self.created)
+    
+    def __init__(self):
+        super(Notebook, self).__init__()
 
 def createMappers():
-    nodemapper = mapper(Node, nodes_table,
+    nodeMapper = mapper(Node, nodesTable,
         properties=dict(inputCells=relation(InputCell, 
                             cascade="all, delete-orphan"),
                         textCells=relation(TextCell, 
                             cascade="all, delete-orphan"),
                         children=relation(Node, 
-                            cascade="all, delete-orphan", backref='parent')))
-    cellmapper = mapper(Cell, cells_table)
-    inputcellmapper = mapper(InputCell, input_cells_table, inherits=cellmapper)
-    textcellmapper = mapper(TextCell, text_cells_table, inherits=cellmapper)
-    usermapper = mapper(User, users_table,
+                            cascade="all", backref='parent')))
+    cellMapper = mapper(Cell, cellsTable)
+    inputcellMapper = mapper(InputCell, inputCellsTable, inherits=cellMapper)
+    textcellMapper = mapper(TextCell, textCellsTable, inherits=cellMapper)
+    userMapper = mapper(User, usersTable,
         properties=dict(notebooks=relation(Notebook, 
             cascade="all, delete-orphan", backref='user')))
-    notebookmapper = mapper(Notebook, notebooks_table, 
+    notebookMapper = mapper(Notebook, notebooksTable, 
         properties=dict(root=relation(Node, uselist=False)))
-    
-    # usermapper.add_property('notebooks', relation(Notebook))
 
     
 # Initialization
@@ -155,21 +127,32 @@ createMappers()
 
 session = create_session()
 
-# bgranger = User()
-# bgranger.username = 'bgranger'
-# bgranger.email = 'ellisonbg@gmail.com'
-# session.save(bgranger)
-# 
-# nb0 = Notebook()
-# nb0.user_id
+def createUser(session):
+    # Create user
+    u = User()
+    u.username = 'bgranger'
+    u.email = 'ellisonbg@gmail.com'
+    session.save(u)
+    session.flush()
+    return u
+    
+def createNotebook(session, user):
+    nb = Notebook()
+    nb.user = user
+    session.save(nb)
+    session.flush()
+    return nb
 
-#c = Cell()
-#c.input = 'foo'
-#c.output = 'bar'
+u = createUser(session)
+nb = createNotebook(session, u)
 
-#session.save(c)
-
-#session.flush()
+# c0 = InputCell()
+# c0.notebook = nb
+# c0.input = 'import time'
+# c0.output = ''
+# nb.root = c0
+# session.save(c0)
+# session.flush()
 
 
     
