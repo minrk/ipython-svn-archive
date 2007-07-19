5928e56b1285591cfdd1647a1c9e28209347fae1
Fernando Pérez <fperez.net@gmail.com>
2007-07-19 22:45:32 +0000

    Code is mostly working, I'll try to get the ReadOnly stuff working more cleanly.
    git-svn-id: http://ipython.scipy.org/svn/ipython@2516

--- ipython/branches/saw/sandbox/tconfig.py
+++ ipython/branches/saw/sandbox/tconfig.py
@@ -2,27 +2,84 @@
 """
 
 # Stdlib imports
+from inspect import isclass
+#from pprint import pprint  # dbg
 
 # External imports
-from enthought.traits.api import HasTraits, HasStrictTraits, Trait, TraitError,\
-     CInt, CFloat, String
+from enthought.traits.api import HasTraits, HasStrictTraits, MetaHasTraits, \
+     Trait, TraitError, Bool, Int, Float, Str, ReadOnly, ListFloat
 
-from configobj import ConfigObj
+import configobj
 
 # Code begins
 
 # Utility functions
-def get_traits(obj):
+def get_scalars(obj):
     skip = set(['trait_added','trait_modified'])
     return [k for k in obj.__class_traits__ if k not in skip]
 
+def get_sections(obj,sectionClass):
+    return [(n,v) for (n,v) in obj.__dict__.iteritems()
+            if isclass(v) and issubclass(v,sectionClass)]
+
+def get_instance_scalars(obj):
+    return [k for k,v in obj.__dict__.iteritems()
+            if not isinstance(v,TConfigSection)]
+
+def get_instance_sections(obj):
+    return [v for k,v in obj.__dict__.iteritems()
+            if isinstance(v,TConfigSection)]
+
+def tsecDump(tconf,depth):
+    
+    indent = '    '*max(depth-1,0)
+    
+    top_name = tconf.__class__.__name__
+    if depth == 0:
+        label = '# Dump of %s\n' % top_name
+    else:
+        label = '\n'+indent+('[' * depth) + top_name + (']'*depth)
+
+    out = [label]
+
+    for s in get_instance_scalars(tconf):
+        v = getattr(tconf,s)
+        out.append(indent+('%s = %r' % (s,v)))
+
+    for sec in get_instance_sections(tconf):
+        out.extend(tsecDump(sec,depth+1))
+
+    return out
+    
+
+def tconfDump(tconf):
+    """Dump a tconf object to a string"""
+
+    tstr = []
+    tstr.extend(tsecDump(tconf,depth=0))
+    return '\n'.join(tstr)
+
+def tconfPrint(tconf):
+    print tconfDump(tconf)
+    
+
+# Use a simple factory that wraps our option choices for using ConfigObj.  I'm
+# hard-wiring certain choices here, so we'll always use instances with THESE
+# choices.
+def mkConfigObj(filename):
+    return configobj.ConfigObj(filename,
+                               file_error=True,
+                               interpolation='Template',
+                               unrepr=True)
 
 # Main TConfig class and supporting exceptions
+class TConfigSection(HasTraits): pass
+
 class TConfigError(Exception): pass
 
 class TConfigInvalidKeyError(TConfigError): pass
 
-class TConfig(HasStrictTraits):
+class TConfig(TConfigSection):
     """A class representing configuration objects.
 
     Note: this class should NOT have any traits itself, since the actual traits
@@ -32,16 +89,24 @@ class TConfig(HasStrictTraits):
     def __init__(self,config):
         """Makes a Traited config object out of a ConfigObj instance
         """
-        # Validate the set of keys
-        my_traits = set(get_traits(self))
-        conf_keys = set(config.keys())
-        invalid_keys = conf_keys - my_traits
-        if invalid_keys:
-            raise TConfigInvalidKeyError("Invalid keys: %s" % invalid_keys)
+        # Validate the set of scalars ...
+        my_scalars = set(get_scalars(self))
+        cf_scalars = set(config.scalars)
+        invalid_scalars = cf_scalars - my_scalars
+        if invalid_scalars:
+            raise TConfigInvalidKeyError("Invalid keys: %s" % invalid_scalars)
+        # ... and sections
+        section_items = get_sections(self.__class__,TConfig)
+        my_sections = set([n for n,v in section_items])
+        cf_sections = set(config.sections)
+        invalid_sections = cf_sections - my_sections
+        if invalid_sections:
+            raise TConfigInvalidKeyError("Invalid sections: %s" %
+                                         invalid_sections)
 
         # Now set the traits based on the config
         try:
-            for k in conf_keys:
+            for k in cf_scalars:
                 setattr(self,k,config[k])
         except TraitError,e:
             t = self.__class_traits__[k]
@@ -49,29 +114,114 @@ class TConfig(HasStrictTraits):
             msg += "Expected type: %s" % t.handler.info()
             raise TConfigError(msg)
 
-# Example simple configuration
-class MyConfig(TConfig):
-    n = CInt
-    x = CFloat
-    s = String
-    
-# Example app using this
-class App(object):
-    def __init__(self,init_only_config,init_config):
-        self.rcx = MyConfig(init_only_config)
-        self.rc = MyConfig(init_config)
+        # And build subsections
+        for s,v in section_items:
+            section = v(config[s])
+            if issubclass(v,ReadOnlyTConfig):
+                ro = ReadOnlyTConfig(section)
+                # XXX - Hack the name back in place.  This should be fixed and
+                # done more cleanly via a proper inheritance hierarchy, but I
+                # kept having problems with that approach due to the fact that
+                # the ReadOnly class needs to create a bunch of ReadOnly traits
+                # out of traits that have already been validated.  So it
+                # fundamentally needs to be a different class
+                ro.__class__.__name__ = s
+                setattr(self,s,ro)
+            else:
+                setattr(self,s,section)
 
+class ReadOnlyTConfig(TConfigSection):
+    def __init__(self,tconf):
+        tctraits = get_scalars(tconf)
+        for t in tctraits:
+            self.add_trait(t,ReadOnly)
+            setattr(self,t,getattr(tconf,t))
 
-# Testing
+# Testing/example
 if __name__ == '__main__':
 
-    # Normal tests
-    good_config = dict(n='3',x='4.15',s='Python')
-    t1 = MyConfig(good_config)
-    
-    fname = 'tconfig1.ini'
-    conf = ConfigObj(fname)
-    t2 = MyConfig(conf)
+    class App(object):
+        """A trivial 'application' class to be initialized.
+        """
+        def __init__(self,configClass,conf_filename):
+            conf = mkConfigObj(conf_filename)
+            self.rc = configClass(conf)
+
+    # Example of an application configuration and the app using it
+    class AppConfig(TConfig):
+
+        m = Int
+
+        class InitOnly(TConfig,ReadOnlyTConfig):
+            n = Int
+            x = Float
+
+        class Protocol(TConfig):
+            ptype = Str
+
+            class Handler(TConfig):
+                key = Str
+
+        class Machine(TConfig):
+            ip = Str
+            port = Int
+
+    simpleapp = App(AppConfig,'tconfig2.conf')
+
+
+    # A matplotlib-like example
+
+    standard_color = Trait ('black',
+                            {'black': (0.0, 0.0, 0.0, 1.0),
+                             'blue': (0.0, 0.0, 1.0, 1.0),
+                             'cyan': (0.0, 1.0, 1.0, 1.0),
+                             'green': (0.0, 1.0, 0.0, 1.0),
+                             'magenta': (1.0, 0.0, 1.0, 1.0),
+                             'orange': (0.8, 0.196, 0.196, 1.0),
+                             'purple': (0.69, 0.0, 1.0, 1.0),
+                             'red': (1.0, 0.0, 0.0, 1.0),
+                             'violet': (0.31, 0.184, 0.31, 1.0),
+                             'yellow': (1.0, 1.0, 0.0, 1.0),
+                             'white': (1.0, 1.0, 1.0, 1.0),
+                             'transparent': (1.0, 1.0, 1.0, 0.0) } )
+
+    class MPLConfig(TConfig):
+
+        # Valid backends, first is default
+        backend = Trait('TkAgg','WXAgg','GTKAgg','QtAgg','Qt4Agg')
+        interactive = Bool(False)
+        
+        class InitOnly(TConfig,ReadOnlyTConfig):
+            """Things that can only be set at init time"""
+            numerix = Str('numpy')
+
+        class lines(TConfig):
+            linewidth = Float(2.0)
+            linestyle = Trait('-','=','^')
+
+        class figure(TConfig):
+            figsize = ListFloat([6.4,4.8])  # figure size in inches
+            dpi = Int(100)            # figure dots per inch
+            facecolor = Float(0.75)    # figure facecolor; 0.75 is scalar gray
+            edgecolor = Trait('white',standard_color)
+
+            class subplot(TConfig):
+                """The figure subplot parameters.  All dimensions are fraction
+                of the figure width or height"""
+                left = Float(0.125)
+                right = Float(0.9)
+                bottom = Float(0.1)
+                top = Float(0.9)
+
+    mpl = App(MPLConfig,'mpl.conf')
+    print "Play with the 'mpl' object a little, esp its .rc attribute..."
+    print "You can even do mpl.rc.edit_traits() if you are running in "
+    print "ipython -wthread.  It only works with the top-level for now."
+    print
+    print "The following is an auto-generated dump of the rc object."
+    print "Note that this remains valid input for an rc file:"
+    print
+    tconfPrint(mpl.rc)
 
     # A few exception-raising tests, turn this later into a doctest that
     # actually runs them, once we settle the exception hirerarchy and format
