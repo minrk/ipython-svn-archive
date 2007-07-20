bcf0841118bac56109a169980251bcbd7284be04
Fernando Pérez <fperez.net@gmail.com>
2007-07-20 21:51:05 +0000

    Add forgotten file.
    git-svn-id: http://ipython.scipy.org/svn/ipython@2530

---
+++ ipython/branches/saw/sandbox/tconfig/tctst.py
@@ -0,0 +1,30 @@
+"""Little utilities for testing tconfig.
+
+This module is meant to be used via
+
+import tctst; reload(tctst)
+from tctst import *
+
+at the top of the actual test scripts, so that they all get the entire set of
+common test tools with minimal fuss.
+"""
+
+# Standard library imports
+import os
+import sys
+from pprint import pprint
+
+# Our own imports.
+
+import tconfig
+reload(tconfig)
+
+from tconfig import ConfigManager, mkConfigObj
+
+# Useful classes for testing.
+class App(object):
+    """A trivial 'application' class to be initialized.
+    """
+    def __init__(self,configClass,configFilename):
+        self.rcman = ConfigManager(configClass,configFilename)
+        self.rc = self.rcman.tconf
