7c7fba1f0da91e13821783954b2af3d1d56cb061
Ville M. Vainio <vivainio@gmail.com>
2007-08-27 18:22:15 +0000

    update revnum
    git-svn-id: http://ipython.scipy.org/svn/ipython@2677

--- ipython/trunk/IPython/Release.py
+++ ipython/trunk/IPython/Release.py
@@ -22,7 +22,7 @@ name = 'ipython'
 # because bdist_rpm does not accept dashes (an RPM) convention, and
 # bdist_deb does not accept underscores (a Debian convention).
 
-revision = '2665'
+revision = '2676'
 
 version = '0.8.2.svn.r' + revision.rstrip('M')
 
