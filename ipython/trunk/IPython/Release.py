f5d8ad33c91581d6f1edefe3d56cb52d47260004
Ville M. Vainio <vivainio@gmail.com>
2007-08-28 17:51:38 +0000

    ipy_exportdb now tolerates new style aliases'
    git-svn-id: http://ipython.scipy.org/svn/ipython@2681

--- ipython/trunk/IPython/Release.py
+++ ipython/trunk/IPython/Release.py
@@ -22,7 +22,7 @@ name = 'ipython'
 # because bdist_rpm does not accept dashes (an RPM) convention, and
 # bdist_deb does not accept underscores (a Debian convention).
 
-revision = '2676'
+revision = '2680M'
 
 version = '0.8.2.svn.r' + revision.rstrip('M')
 
