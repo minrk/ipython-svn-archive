524df831fe92054a183f5aa371a99db30ea6803b
Brian Granger <ellisonbg@gmail.com>
2007-09-20 17:39:34 +0000

    Modified original setup.py script.
    git-svn-id: http://ipython.scipy.org/svn/ipython@2799

--- ipython/branches/namespaces/ipython1/setup.py
+++ ipython/branches/namespaces/ipython1/setup.py
@@ -18,8 +18,9 @@ setup(
     name = "ipython1",
     version = "0.9alpha2",
     packages = find_packages(),
+    namespace_packages=['ipython1'],
     
-    zip_safe = False,
+    zip_safe = True,
     include_package_data = True,
     entry_points = {
             'console_scripts': ['ipengine = ipython1.scripts.ipengine:start',
