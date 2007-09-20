b8fecd8a473793abbc77b89ca8fd1eca65943ddf
Brian Granger <ellisonbg@gmail.com>
2007-09-20 17:35:50 +0000

    Created __init__.py for ipython namespace package.
    git-svn-id: http://ipython.scipy.org/svn/ipython@2797

--- ipython/branches/namespaces/ipython1/ipython1/__init__.py
+++ ipython/branches/namespaces/ipython1/ipython1/__init__.py
@@ -1,13 +1,5 @@
 # encoding: utf-8
-"""The IPython1 package.
-
-IPython1 represents the next-generation IPython that is being developed.
-
-IPython1 also provides an architecture for interactive parallel computing.
-This architecture is already very functional.  For more information see
-the docstrings for:
-
- - `ipython1.kernel`
- - `ipython1.kernel.api`
-
-"""
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except:
+    pass
