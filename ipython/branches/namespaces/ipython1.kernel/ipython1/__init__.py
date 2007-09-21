f4c5499b74fdbfe8f65175e16391500878ca89d1
Brian Granger <ellisonbg@gmail.com>
2007-09-21 01:36:54 +0000

    Removing the namespaces branch as using namespace packages is up to 3x slower (on NFS) that not using it.
    git-svn-id: http://ipython.scipy.org/svn/ipython@2812

--- ipython/branches/namespaces/ipython1.kernel/ipython1/__init__.py
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except:
-    pass
--- ipython/branches/namespaces/ipython1/ipython1/core1/console.py
+++
@@ -1,6 +0,0 @@
-class LineConsole(object):
-    """Terminal-based, line-oriented console object"""
-
-    def __init__(self):
-        pass
-
