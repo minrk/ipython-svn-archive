48f6b9bb56398b339b237b7b827bc9a226b5e6e1
Fernando Pérez <fperez.net@gmail.com>
2008-02-01 18:00:26 +0000

    Fix 2.5-only syntax accidentally introduced
    git-svn-id: http://ipython.scipy.org/svn/ipython@3006

--- ipython/trunk/IPython/Shell.py
+++ ipython/trunk/IPython/Shell.py
@@ -456,15 +456,14 @@ class MTInteractiveShell(InteractiveShell):
             # CODE_TO_RUN is set to true/false as close as possible to the
             # runcode() call, so that the KBINT handler is correctly informed.
             try:
-               CODE_RUN = True
-               InteractiveShell.runcode(self,code_to_run)
-               if got_lock:
-                  CODE_RUN = False              
-            except KeyboardInterrupt:
-               print "Keyboard interrupted in mainloop"
-               while not self.code_queue.empty():
-                  self.code_queue.get_nowait()
-               break
+                try:
+                   CODE_RUN = True
+                   InteractiveShell.runcode(self,code_to_run)
+                except KeyboardInterrupt:
+                   print "Keyboard interrupted in mainloop"
+                   while not self.code_queue.empty():
+                      self.code_queue.get_nowait()
+                   break
             finally:
                if got_lock:
                   CODE_RUN = False
