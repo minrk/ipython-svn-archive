5a9a457a236897be8283a3b17fc3cc47eaf6bc29
Fernando Pérez <fperez.net@gmail.com>
2008-02-01 07:17:00 +0000

    update revnum to make test release
    git-svn-id: http://ipython.scipy.org/svn/ipython@3002

--- ipython/trunk/IPython/Release.py
+++ ipython/trunk/IPython/Release.py
@@ -22,7 +22,7 @@ name = 'ipython'
 # because bdist_rpm does not accept dashes (an RPM) convention, and
 # bdist_deb does not accept underscores (a Debian convention).
 
-revision = '2919'
+revision = '3001'
 
 version = '0.8.3.svn.r' + revision.rstrip('M')
 
