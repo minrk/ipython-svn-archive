19fc07a11ccca62e972b597f4bf53f8cd3fbc0ee
Brian Granger <ellisonbg@gmail.com>
2008-02-04 21:53:53 +0000

    Removing bad branch
    git-svn-id: http://ipython.scipy.org/svn/ipython@3016

--- ipython1/branches/ipython1-data/sandbox/mpi/mpicontroller.py
+++
@@ -1,7 +0,0 @@
-from mpi4py import MPI as mpi
-import time
-from twisted.internet import reactor
-
-print mpi.rank, mpi.size
-
-reactor.run()
--- ipython1/branches/ipython1-data/sandbox/snakeoil/doc/example/pydt3_inc2.py
+++
@@ -1,9 +0,0 @@
-# More tests
-
-print "Another included file"
-
-1+2
-
-x,y = 3,4
-
-assert x+y==7
